beeb757a251a3693ed9f73b5430702167b696c2f
fengzhizi715 <fengzhizi715@126.com>
2019-05-16 13:42:06 +0800

    update RootCommand

--- src/main/kotlin/cn/netdiscovery/command/RootCommand.kt
+++ src/main/kotlin/cn/netdiscovery/command/RootCommand.kt
@@ -1,7 +1,5 @@
 package cn.netdiscovery.command
 
-import cn.netdiscovery.command.domain.CommandResult
-
 /**
  * Created by tony on 2019-04-26.
  */
@@ -11,35 +9,19 @@ open class RootCommand(
     private val defaultTimeout: Int
 ) : BaseCommand() {
 
-    override fun exec(commands: String): CommandResult {
-        return execute(rootCommand, commands, defaultDirection, defaultTimeout, null)
-    }
+    override fun exec(commands: String) = execute(rootCommand, commands, defaultDirection, defaultTimeout, null)
 
-    override fun exec(commands: String, callback: OutputCallback): CommandResult {
-        return execute(rootCommand, commands, defaultDirection, defaultTimeout, callback)
-    }
+    override fun exec(commands: String, callback: OutputCallback) = execute(rootCommand, commands, defaultDirection, defaultTimeout, callback)
 
-    override fun exec(commands: String, dir: String): CommandResult {
-        return execute(rootCommand, commands, dir, defaultTimeout, null)
-    }
+    override fun exec(commands: String, dir: String) = execute(rootCommand, commands, dir, defaultTimeout, null)
 
-    override fun exec(commands: String, dir: String, callback: OutputCallback): CommandResult {
-        return execute(rootCommand, commands, dir, defaultTimeout, callback)
-    }
+    override fun exec(commands: String, dir: String, callback: OutputCallback) = execute(rootCommand, commands, dir, defaultTimeout, callback)
 
-    override fun exec(commands: String, timeoutInSeconds: Int): CommandResult {
-        return execute(rootCommand, commands, defaultDirection, timeoutInSeconds, null)
-    }
+    override fun exec(commands: String, timeoutInSeconds: Int) = execute(rootCommand, commands, defaultDirection, timeoutInSeconds, null)
 
-    override fun exec(commands: String, timeoutInSeconds: Int, callback: OutputCallback): CommandResult {
-        return execute(rootCommand, commands, defaultDirection, timeoutInSeconds, callback)
-    }
+    override fun exec(commands: String, timeoutInSeconds: Int, callback: OutputCallback) = execute(rootCommand, commands, defaultDirection, timeoutInSeconds, callback)
 
-    override fun exec(commands: String, dir: String, timeoutInSeconds: Int): CommandResult {
-        return execute(rootCommand, commands, dir, timeoutInSeconds, null)
-    }
+    override fun exec(commands: String, dir: String, timeoutInSeconds: Int) = execute(rootCommand, commands, dir, timeoutInSeconds, null)
 
-    override fun exec(commands: String, dir: String, timeoutInSeconds: Int, callback: OutputCallback): CommandResult {
-        return execute(rootCommand, commands, dir, timeoutInSeconds, callback)
-    }
+    override fun exec(commands: String, dir: String, timeoutInSeconds: Int, callback: OutputCallback) = execute(rootCommand, commands, dir, timeoutInSeconds, callback)
 }
