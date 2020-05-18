b846f19edcb393c9e4a6255ba97328a72a43cde0
fengzhizi715 <fengzhizi715@126.com>
2020-05-18 19:55:11 +0800

    update build.gradle

--- src/main/kotlin/cn/netdiscovery/command/Commands.kt
+++
@@ -1,8 +0,0 @@
-package cn.netdiscovery.command
-
-/**
- * Created by tony on 2019-04-26.
- */
-class LinuxCommand : RootCommand("/bin/bash", "/bin", -1)
-
-class WindowsCommand : RootCommand("cmd", null,-1)
--- src/main/kotlin/cn/netdiscovery/command/pipeline/PipelineItem.kt
+++
@@ -1,6 +0,0 @@
-package cn.netdiscovery.command.pipeline
-
-/**
- * Created by tony on 2019-04-28.
- */
-class PipelineItem @JvmOverloads constructor(var name: String?, val order: Int, var command: String? = null)
