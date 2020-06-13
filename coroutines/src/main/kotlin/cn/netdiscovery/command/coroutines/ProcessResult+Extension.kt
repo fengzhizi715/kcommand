1b4e6f3f5f15edfb3529d6ae320865adb912b5f0
fengzhizi715 <fengzhizi715@126.com>
2020-06-13 23:12:55 +0800

    add coroutines module

---
+++ coroutines/src/main/kotlin/cn/netdiscovery/command/coroutines/ProcessResult+Extension.kt
@@ -0,0 +1,19 @@
+package cn.netdiscovery.command.coroutines
+
+import cn.netdiscovery.command.ExecutionResult
+import cn.netdiscovery.command.ProcessResult
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOf
+
+/**
+ *
+ * @FileName:
+ *          cn.netdiscovery.command.coroutines.`ProcessResult+Extension`
+ * @author: Tony Shen
+ * @date: 2020-06-13 23:04
+ * @version: V1.1 <描述当前版本功能>
+ */
+fun ProcessResult.asFlow():Flow<ExecutionResult> {
+
+    return flowOf(this.getExecutionResult())
+}
