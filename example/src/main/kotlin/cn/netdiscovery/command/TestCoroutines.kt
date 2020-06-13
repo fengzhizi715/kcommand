546950425edcceb7b19a2c9dee2f4097575b9b51
fengzhizi715 <fengzhizi715@126.com>
2020-06-13 23:22:50 +0800

    update test case

---
+++ example/src/main/kotlin/cn/netdiscovery/command/TestCoroutines.kt
@@ -0,0 +1,31 @@
+package cn.netdiscovery.command
+
+import cn.netdiscovery.command.coroutines.asFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.runBlocking
+
+/**
+ *
+ * @FileName:
+ *          cn.netdiscovery.command.TestCoroutines
+ * @author: Tony Shen
+ * @date: 2020-06-13 23:15
+ * @version: V1.0 <描述当前版本功能>
+ */
+fun main() = runBlocking{
+
+    val cmd = getPsCmd()
+
+    try {
+        CommandExecutor.execute(cmd, null)
+            .asFlow()
+            .collect{
+                val commandLine = cmd.string()
+                val exitCode = it.exitValue()
+                println("command line: $commandLine\nexecution finished with exit code: $exitCode\n\n")
+            }
+
+    } catch (e: UnrecognisedCmdException) {
+        System.err.println(e)
+    }
+}
