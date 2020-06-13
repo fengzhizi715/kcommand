d622749e211f3866d2c4b436c71820a46a81cc58
fengzhizi715 <fengzhizi715@126.com>
2020-06-13 10:56:54 +0800

    update Result

--- core/src/main/kotlin/cn/netdiscovery/command/function/Result.kt
+++ core/src/main/kotlin/cn/netdiscovery/command/function/Result.kt
@@ -32,3 +32,17 @@ fun <T> Result<T, T>.get() = when (this) {
     is Success<T> -> value
     is Failure<T> -> reason
 }
+
+infix fun <T, E> Result<T, E>.or(block: T) = when (this) {
+    is Success -> this
+    else -> Success(block)
+}
+
+inline infix fun <T, E> Result<T, E>.getOrElse(block: (E) -> T): T {
+    return when (this) {
+        is Success -> value
+        is Failure -> block(reason)
+    }
+}
+
+inline fun <T, E> Result<T, E>.peek(f: (T) -> Unit) = apply { if (this is Success<T>) f(value) }
