f6d8441524f48edb8c101b8ce9c732431813cc9d
fengzhizi715 <fengzhizi715@126.com>
2020-10-02 10:52:47 +0800

    增加注释

--- core/src/main/kotlin/cn/netdiscovery/command/UnrecognisedCmdException.kt
+++ core/src/main/kotlin/cn/netdiscovery/command/UnrecognisedCmdException.kt
@@ -8,7 +8,7 @@ import java.io.IOException
  *          cn.netdiscovery.command.UnrecognisedCmdException
  * @author: Tony Shen
  * @date: 2020-05-19 17:49
- * @version: V1.0 <描述当前版本功能>
+ * @version: V1.0 封装的异常类
  */
 class UnrecognisedCmdException(private val cmd: String) : IOException() {
 
